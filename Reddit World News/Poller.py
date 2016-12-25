316c1502c76877056509b4b451d4b40683629faa
Teddy Todorov <forteddyt@gmail.com>
2016-12-24 22:40:09 -0500

    Appropriate comments and functional writing to files

--- Reddit World News/Poller.py
+++ Reddit World News/Poller.py
@@ -1,8 +1,12 @@
 # PRAW - https://praw.readthedocs.io/en/latest/index.html
 # Script is ran at approximately 11:59 P.M. EST each day
+# Formatting into a .docx is done elsewhere
 
 import praw
 import datetime
+
+# Max amount of posts to collect per day
+num_posts = 10
 
 # Obtains a Reddit instance
 # client_id and client_secret are reddit user's application's credentials
@@ -15,18 +19,24 @@ reddit = praw.Reddit(client_id='1sBmgGrhe_PtkA',
 subreddit_worldnews = reddit.subreddit('worldnews')
 
 # Defines a dictionary of search terms to search for. Not case-sensitive
-search_terms = {'Saudi Arabia'}
+search_terms = {"Saudi Arabia"}
 
+# Used to determine the starting point of weekly polling intervals
+# ie. If date in base.txt is 12/20/16 the intervals would start from the 20th
 base_txt = open("base.txt", "r")
 increment_date = base_txt.readline()
 base_txt.close()
 
+# Creates a start_date and end_date based off base.txt file
 start_date = datetime.datetime.strptime(increment_date, "%m/%d/%y")
 end_date = start_date + datetime.timedelta(days=6)
 
+# Terminal date is a day after the end date
 cur_date = datetime.datetime.strptime(datetime.date.today().strftime("%m/%d/%y"), "%m/%d/%y")
 terminate_date = end_date + datetime.timedelta(days=1)
 
+# Determines if the next interval cycle is to begin
+# Overwrites base.txt with appropriate date
 if cur_date == terminate_date:
 	start_date = cur_date
 
@@ -37,27 +47,35 @@ if cur_date == terminate_date:
 	end_date = start_date + datetime.timedelta(days=6)
 
 
+# Creates a string representation of the dates in MM.DD.YY format
 str_start_date = str(start_date.month) + "." + str(start_date.day) +"." + str(start_date.year)
 str_end_date = str(end_date.month) + "." + str(end_date.day) + "." + str(end_date.year)
 
+# Creates a string from the start to the end date
+timeframe = str_start_date + " to " + str_end_date
 
-timeframe = str_start_date + " - " + str_end_date + ".txt"
-weekly_draft = open(timeframe, "a")
+# Creates and opens a weekly .txt file 
+weekly_title = "Global Events - " + timeframe + ".txt"
+weekly_draft = open(weekly_title, "a")
 
 # Obtains a Submission instance from a specified Subreddit category
-for submission in subreddit_worldnews.top('day', limit = 10):
-	weekly_draft.write("--- " + submission.title + " ---\n")
+for submission in subreddit_worldnews.top('day', limit = num_posts):
+	weekly_draft.write(submission.title + "\n")
 	weekly_draft.write(submission.url + "\n\n")
 
 weekly_draft.close()
 
+# Creates and opens a weekly, topic-specific .txt file
+# Topic-specific refers to the topics in the search_terms dictionary
+topic_title = "Topic Specific Events - " + timeframe + ".txt"
+topic_draft = open(topic_title, "a")
 
 # Search /r/worldnews for articles containing a search_terms term
 # Only searches for articles created in past 24 hours
 for term in search_terms:
-	print("Search results for \"" + term + "\"")
+	topic_draft.write("Search results for \"" + term + "\" - " + str(cur_date) + "\n")
 	for submission in subreddit_worldnews.search(term, 'relevance', 'cloudsearch', 'day'):
-		print('~~~ ' + submission.title + " ~~~")
-		print(submission.url)
-	print("")
+		topic_draft.write(submission.title + "\n")
+		topic_draft.write(submission.url + "\n\n")
 
+topic_draft.close()
