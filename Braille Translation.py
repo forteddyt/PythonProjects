12da4ef68c27c5755a8baf5eef3e9dafe42de04d
Teddy Todorov <forteddyt@gmail.com>
2016-12-25 19:11:00 -0500

    foobar Braille Translation

---
+++ Braille Translation.py
@@ -0,0 +1,20 @@
+import sys
+
+def answer(plaintext):
+    # your code here
+    lookup = {'^' :'000001','_' : '000000','a':'100000','b':'110000',
+                'c' :'100100','d':'100110','e':'100010','f':'110100',
+                'g':'110110','h':'110010','i':'010100','j':'010110',
+                'k':'101000','l':'111000','m':'101100','n':'101110',
+                'o':'101010','p':'111100','q':'111110','r':'111010',
+                's':'011100','t':'011110','u':'101001','v':'111001',
+                'w': '010111','x':'101101','y':'101111','z':'101011'}
+    plaintext = plaintext.replace(' ', '_')
+    for l in plaintext:
+        sys.stdout.write(l + ' : ')
+        if l.isupper():
+            sys.stdout.write(lookup['^'])
+        sys.stdout.write(lookup[l.lower()])
+        print('')
+
+answer('code')
