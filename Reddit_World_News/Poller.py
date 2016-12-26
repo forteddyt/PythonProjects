531dca6518c76d207c158992d5d37018b7888cc5
Teddy Todorov <forteddyt@gmail.com>
2016-12-25 19:27:34 -0500

    +PrintFlush function now utilized

--- Reddit_World_News/Poller.py
+++ Reddit_World_News/Poller.py
@@ -2,12 +2,19 @@
 # Script is ran at approximately 11:59 P.M. EST each day
 # Formatting into a .docx is done elsewhere
 
+import sys
+
+# A simple print with a system flush afterwards
+def printFlush(string = ''):
+	print(string)
+	sys.stdout.flush()
+
 # Ensures the PRAW package is installed
 try:
 	import praw
 except ImportError as e:
 	# Asks user to install PRAW if they do not have it
-	print ("The Python Reddit API Wrapper (PRAW) package is not currently installed on this machine."
+	printFlush("The Python Reddit API Wrapper (PRAW) package is not currently installed on this machine."
 			+ "\n The PRAW package is needed in order for this script to run.")
 	resp = input("Would you like to install the PRAW package?\n\t[Y]es or [N]o:")
 	
@@ -17,16 +24,16 @@ except ImportError as e:
 		resp = input("Invalid response.\nWould you like to install the PRAW package?\n\t[Y]es or [N]o: ")
 	
 	# Install PRAW with pip if the user allows it, exit the script otherwise
-	if resp.upper() == "Y" or resp.upper() == "YES":
+	if (resp.upper() == "Y" or resp.upper() == "YES"):
+		printFlush("Installing PRAW...")
 		import pip
 		pip.main(['install', 'praw'])
 		import praw
+		printFlush("**PRAW installed.")
 	else:
-		import sys
 		import time
-		print("Unable to run script without appropriate packages.\n**EXITING SCRIPT**")
-		sys.stdout.flush()
-		time.sleep(2.25)
+		printFlush("Unable to run script without appropriate packages.\n")
+		time.sleep(2)
 		sys.exit(0)
 
 import datetime
@@ -38,7 +45,7 @@ file_path = os.path.dirname(os.path.realpath(__file__)) + "\\"
 # Max amount of posts to collect per day
 num_posts = 10
 
-print("Starting script...")
+printFlush("Starting script...")
 
 # Obtains a Reddit instance
 # client_id and client_secret are reddit user's application's credentials
@@ -53,18 +60,18 @@ subreddit_worldnews = reddit.subreddit('worldnews')
 # Defines a dictionary of search terms to search for. Not case-sensitive
 search_terms = {"Saudi Arabia"}
 
-print("Obtaining base information...")
+printFlush("Obtaining base information...")
 
 # Used to determine the starting point of weekly polling intervals
 # ie. If date in base.txt is 12/20/16 the intervals would start from the 20th
 try:
 	base_txt = open(file_path + "base.txt", "r")
 except Exception as e:
-	print("No base.txt found, creating...")
+	printFlush("No base.txt found, creating...")
 	base_txt = open(file_path + "base.txt", "w")
 	base_txt.close()
 	base_txt = open(file_path + "base.txt", "r")
-	print("**base.txt created.")
+	printFlush("**base.txt created.")
 increment_date = base_txt.readline()
 base_txt.close()
 
@@ -74,14 +81,14 @@ cur_date = datetime.datetime.strptime(datetime.date.today().strftime("%m/%d/%y")
 # Determines if there is data in the base.txt file
 # Writes to file and sets current date (DD/MM/YY format) as increment_date 
 if increment_date == "":
-	print("No initial date found, setting '" + cur_date.strftime('%x') + "' as initial date...")
+	printFlush("No initial date found, setting '" + cur_date.strftime('%x') + "' as initial date...")
 	increment_date = str(cur_date.strftime('%x'))
 	base_txt = open(file_path + "base.txt", "a")
 	base_txt.write(cur_date.strftime('%x'))
 	base_txt.close()
-	print("**Initial date is now set.")
+	printFlush("**Initial date is now set.")
 
-print("**Base information obtained.")
+printFlush("**Base information obtained.")
 
 # Creates a start_date and end_date based off base.txt file
 start_date = datetime.datetime.strptime(increment_date, "%m/%d/%y")
@@ -90,24 +97,24 @@ end_date = start_date + datetime.timedelta(days=6)
 # Terminal date is a day after the end date
 terminate_date = end_date + datetime.timedelta(days=1)
 
-print("Checking new date cycle...")
+printFlush("Checking new date cycle...")
 
 # Determines if the next interval cycle is to begin
 # Overwrites base.txt with appropriate date
 if cur_date == terminate_date:
 	start_date = cur_date
 
-	print("Creating new cycle...")
+	printFlush("Creating new cycle...")
 
 	base_txt = open(file_path + "base.txt", "w")
 	base_txt.write(start_date)
 	base_txt.close()
 
-	print("**New cycle create.")
+	printFlush("**New cycle create.")
 
 	end_date = start_date + datetime.timedelta(days=6)
 else:
-	print("**No new cycle needed.")
+	printFlush("**No new cycle needed.")
 
 
 # Creates a string representation of the dates in MM.DD.YY format
@@ -117,7 +124,7 @@ str_end_date = str(end_date.month) + "." + str(end_date.day) + "." + str(end_dat
 # Creates a string from the start to the end date
 timeframe = str_start_date + " to " + str_end_date
 
-print("Starting general searches...")
+printFlush("Starting general searches...")
 
 # Creates and opens a weekly .txt file 
 weekly_title = "Global Events - " + timeframe + ".txt"
@@ -129,13 +136,13 @@ index = 1
 for submission in subreddit_worldnews.top('day', limit = num_posts):
 	weekly_draft.write(submission.title + "\n")
 	weekly_draft.write(submission.url + "\n\n")
-	print(str(index) + " written to file...")
+	printFlush(str(index) + " written to file...")
 	index += 1
 
 weekly_draft.close()
 
-print("**General searches complete.")
-print("Starting topic-specific searches...")
+printFlush("**General searches complete.")
+printFlush("Starting topic-specific searches...")
 
 index = 1
 topic_index = 1
@@ -152,13 +159,13 @@ for term in search_terms:
 	for submission in subreddit_worldnews.search(term, 'relevance', 'cloudsearch', 'day'):
 		topic_draft.write(submission.title + "\n")
 		topic_draft.write(submission.url + "\n\n")
-		print("Topic " + str(topic_index) + ", index " + str(index) + " writtin to file...")
+		printFlush("Topic " + str(topic_index) + ", index " + str(index) + " writtin to file...")
 		index += 1
 	topic_index += 1
 	index = 1
 
 topic_draft.close()
-print("**Topic-specific searches complete.")
+printFlush("**Topic-specific searches complete.")
 
 
-print("**Script complete.")
+printFlush("**Script complete.")
