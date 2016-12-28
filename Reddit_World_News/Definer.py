6df8eeaa3f386f8a07974cd05e2bc82818ffe937
forteddyt <forteddyt@gmail.com>
2016-12-27 22:46:25 -0500

    close function works

--- Reddit_World_News/Definer.py
+++ Reddit_World_News/Definer.py
@@ -4,4 +4,30 @@
 # Definer edits the base.txt file, line 2
 
 import os
+import sys
 file_path = os.path.dirname(os.path.realpath(__file__)) + "\\" # Obtains the scripts file path
+polling = True
+
+# A simple print with a system flush afterwards
+def printFlush(string = ''):
+	print(string)
+	sys.stdout.flush()
+
+def poll_user():
+	return input(">>")
+
+def definerHelp():
+	pass
+
+def close():
+	printFlush("Closing Definer.py")
+	global polling
+	polling = False
+
+call_list = {'close' : close,'help' : definerHelp}
+while polling == True:
+	try:
+		command = poll_user()
+		call_list.get(command)()
+	except TypeError as e:
+		printFlush(command + " command not found")
